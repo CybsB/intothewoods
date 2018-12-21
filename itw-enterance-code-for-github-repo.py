74d05e21acf6474a50a9f98bd6701eea8ada4206
CybsB <37172568+CybsB@users.noreply.github.com>
2018-12-21 21:05:18 +0000

    NB:this is only code enterance code V1 which only allow for entrance and this does not check the banned list

--- itw-enterance-code-for-github-repo.py
+++ itw-enterance-code-for-github-repo.py
@@ -1,1 +1,33 @@
-d
+#part of into the woods
+import datetime
+import os
+while True:
+    command="wget -N http://212.71.248.238/newfile.txt"
+    os.system(command)
+
+    mode= open("newfile.txt", "r")
+    choice=mode.read()
+    print(choice)
+    if choice=="E":
+        
+        print("DEBUG: ENTER MODE SELECTED")
+        #entering woods
+        
+        pers=str(input("DEBUG: AWAITING CARD'S BARCODE"))
+        print("DEBUG: searching database for barcode: "+pers)
+        searchfile = open("db.csv", "r")
+        for line in searchfile:
+             if "searchphrase" in line: print line
+             person=line
+        searchfile.close()
+        print("DEBUG:opening in.csv file")
+        f= open("in.csv","a")
+        f.write(str(datetime.datetime.now().time())+","+str(pers)+","+str(person))
+        f.close()
+        com="rm newfile.txt"
+        #clear up for next go
+        os.system(com)
+   # else:
+	   #os.system("rm newfile.txt")
+        
+        
